bd5f85627868ba784f045ab50aa3e130149cff64
soulemanou kunchiefuh babanou <kunchiefuh@gmail.com>
2023-02-18 21:28:02 +0100

    some rerors thaty needed modifications

--- app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-class Recipe < ApplicationRecord
-  belongs_to :user
-  has_many :recipes_foods, dependent: :destroy, class_name: 'RecipeFood'
-
-  validates :name, :preparation_time, :cooking_time, :description, presence: true
-end
--- spec/models/recipe_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
